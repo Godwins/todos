da660a38c54350959221101120b98ccd1bd22ddc
Godwins <twesigyegodwin@gmail.com>
2019-08-10 19:20:02 +0300

    initial commit

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import { createStackNavigator, createAppContainer, createSwitchNavigator } from "react-navigation";
 import Home from '../../components/home';
 import Details from '../../components/details';
+import Login from '../../components/login'
 
 const AppNavigator = createStackNavigator({
   Home,
+  Login,
   Details
 });
 
